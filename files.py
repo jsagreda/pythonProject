75dc74c34bdf0ebdab7b851b967284228c467c11
jsagreda <83132728+jsagreda@users.noreply.github.com>
2021-04-24 16:30:32 -0500

    Restablecer UTF

--- files.py
+++ files.py
@@ -1,7 +1,7 @@
 # files.py
 import sys
 
-f = open(sys.argv[1], mode='rt', encoding='utf-32')
+f = open(sys.argv[1], mode='rt', encoding='utf-8')
 for line in f:
     sys.stdout.write(line)
 f.close()
